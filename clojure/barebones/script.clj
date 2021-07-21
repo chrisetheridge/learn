323bd18e0f587789e523b724e451f564c5c9b1d9
chris etheridge <chrise@cognician.com>
2021-07-21 19:37:41 +0200

    Fix script

--- clojure/barebones/script.clj
+++ clojure/barebones/script.clj
@@ -3,26 +3,31 @@
 (require '[clojure.java.io :as io]
          '[clojure.string :as string])
 
-(def files (apply concat
-                  [(io/file "deps.edn")]
-                  (filter #(.isFile %) (file-seq (io/file "src")))
-                  (filter #(.isFile %) (file-seq (io/file "dev")))))
+(def script-location
+  (string/join "/" (butlast (string/split *file* #"/"))))
 
-(def paths (map #(.getPath %) files))
+(def files
+  ["deps.edn"
+   "src/{{PROJECT_NAME}}/core.clj"
+   "src/{{PROJECT_NAME}}/repl.clj"]
+  #_(apply concat
+           [(io/file "deps.edn")]
+           (filter #(.isFile %) (file-seq (io/file "src")))
+           (filter #(.isFile %) (file-seq (io/file "dev")))))
 
 (def PROJECT_NAME_RX #"\{\{PROJECT_NAME\}\}")
 
 (defn replace-project-name [s project-name]
   (string/replace s PROJECT_NAME_RX project-name))
 
-(defn process [[output-dir project-name] paths]
-  (doseq [p paths]
+(defn process [[output-dir project-name] files]
+  (doseq [f files]
     (let [real-p (str output-dir "/"
                       project-name "/"
-                      (replace-project-name p project-name))]
-      (println "Creating" real-p)
+                      (replace-project-name f project-name))]
+      (println "Creating" real-p "from" f)
       (io/make-parents real-p)
-      (spit real-p (replace-project-name (slurp p) project-name)))))
+      (spit real-p (replace-project-name (slurp (str script-location "/" f)) project-name)))))
 
 ;; bb script.clj /path/for/new/project new-project-name
-(process *command-line-args* paths)
+(process *command-line-args* files)
